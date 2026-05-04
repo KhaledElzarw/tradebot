2bd39eba6cc85ddeccb03489066ba174af1db5f5
KhaledElzarw <KhaledElzarw@users.noreply.github.com>
2026-05-04 12:15:36 +0000

    chore: remove legacy baserow scripts

--- tests/test_baserow_sync.py
+++
@@ -1,9 +0,0 @@
-import baserow_sync
-
-
-def test_baserow_sync_disabled_without_token(monkeypatch):
-    monkeypatch.setattr(baserow_sync, "TOKEN", "")
-
-    syncer = baserow_sync.BaserowSync()
-
-    assert syncer.enabled is False
